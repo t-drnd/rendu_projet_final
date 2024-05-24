7fc7c563736129fd6e4149f2866583010507ff6d
t-drnd <148848242+t-drnd@users.noreply.github.com>
2024-05-24 09:08:07 +0200

    Update booster.js

--- front/booster.js
+++ front/booster.js
@@ -1,27 +1,3 @@
-// const booster_booster = document.querySelector(".booster_cards")
-
-// const booster = async () => {
-//     async function fetchCards() {
-//         const url = "https://hp-api.lainocs.fr/characters";
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         return data;
-//     }
-
-//     for (i=0; i<5; i++) {
-//         let random = Math.floor(Math.random() * 30)
-//         const cards = await fetchCards();
-//         const card = cards[random]
-//         booster_booster.innerHTML += `
-//         <div class="booster_card">
-//             <h2>${card.name}</h2>
-//             <img src="${card.image}" alt="">
-//           </div>`
-//     }
-// }
-
-// booster()
-
 const booster_booster = document.querySelector(".booster_cards");
 
 const booster = async () => {
@@ -32,7 +8,7 @@ const booster = async () => {
         return data;
     }
 
-    const cards = await fetchCards();  // Récupérer toutes les cartes une seule fois
+    const cards = await fetchCards();
 
     for (let i = 0; i < 5; i++) {
         let random = Math.floor(Math.random() * cards.length);
@@ -46,11 +22,9 @@ const booster = async () => {
         </div>`;
     }
 
-    // Afficher la section des cartes une fois qu'elles ont été générées
     document.querySelector(".booster").style.display = "block";
 };
 
-// Écouteur d'événement pour le clic sur le bouton
 document.querySelector(".booster-button").addEventListener("click", () => {
-    booster(); // Appel de la fonction pour afficher les cartes
+    booster();
 });
